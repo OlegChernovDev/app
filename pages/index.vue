cef4ef8f6fd8ee9db1f4e08f6cf56196c2e7cbd5
Oleg <oleg.chernoidewofkrewfewunf@ksdas.dff>
2022-04-04 08:30:46 +0300

    add papercss

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,28 @@
 <template>
-  <div>
-    <button @click="test">Test</button>
-  </div>
+	<div class="container">
+		<button @click="test">Test</button>
+	</div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage',
-  methods: {
-    test() {
-      fetch('http://localhost:3000/getCourses').then(res => console.log(res));
-    }
-  }
-}
+	name: "IndexPage",
+	methods: {
+		test() {
+			fetch("http://localhost:3000/getCourses").then((res) =>
+				console.log(res)
+			);
+		},
+	},
+};
 </script>
+<style scoped>
+.container {
+	max-width: 640px;
+	min-height: 100vh;
+	padding: 50px 0;
+	margin: 0 auto;
+	box-shadow: 0 0 1px black;
+	overflow: hidden;
+}
+</style>
